eb946a7a6f7134233101207286b66fb3416a995c
Craig Findlater <craig.findlater@focus.net.nz>
2022-12-24 18:17:51 +1300

    day 9 - part 1

--- AdventOfCode2022/Day09/Day09.cs
+++ AdventOfCode2022/Day09/Day09.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace AdventCalendar;
 
@@ -6,10 +7,10 @@ public class Day09
 {
     public static void Part1()
     {
-        //var instructions = File.ReadAllText($"{Environment.CurrentDirectory}\\day09\\input-day9.txt");
         var instructions = File.ReadAllText($"{Environment.CurrentDirectory}\\day09\\input-day9.txt");
+        //var instructions = File.ReadAllText($"{Environment.CurrentDirectory}\\day09\\input-day9-demo.txt");
 
-        var rope = new Rope(50, 50);
+        var rope = new Rope(0, 0);
 
         foreach (string data in instructions.Split("\r\n"))
         {
@@ -19,7 +20,7 @@ public class Day09
         }
 
         Console.WriteLine("Day 9 - Part 1");
-        Console.WriteLine(rope.TailPositions);
+        Console.WriteLine(rope.TailPositions.Count);
         Console.WriteLine();
     }
 
@@ -35,39 +36,143 @@ public class Day09
     {
         public Position Head { get; private set; }
         public Position Tail { get; private set; }
-
+    
         public Rope(int startX, int startY)
         {
             Head = new Position(startX, startY);
             Tail = new Position(startX, startY);
+            TailPositions = new List<Position>();
+            TailPositions.Add(new Position(Tail.X, Tail.Y));
         }
 
-        public int TailPositions { get; protected set; }
+        public List<Position> TailPositions { get; protected set; }
 
         public void Move(string direction, int distance)
-        { 
-            switch (direction)
+        {
+            for (var i = 0; i < distance; i++)
             {
-                case "L":
-                    //this.Head -= 1;
-                    break;
+                switch (direction)
+                {
+                    case "L":
+                        Head.X--;                       
+                        break;
 
-                case "R":
-                    break;
+                    case "R":
+                        Head.X++;                        
+                        break;
 
-                case "U":
-                    break;
+                    case "U":
+                        Head.Y--;                       
+                        break;
 
-                case "D":
-                    break;
-
+                    case "D":
+                        Head.Y++;                       
+                        break;
+                }
+                
+                if (!Head.IsAdjacent(Tail)) MakeAdjacent();
             }
         }
+
+        private void MakeAdjacent()
+        {
+            // in the same row
+            if (Head.X == Tail.X)
+            {
+                if (Head.X - Tail.X >= 2)
+                {
+                    Tail.X++;
+                }
+                else if (Head.X - Tail.X <= 2)
+                {
+                    Tail.X--;
+                }
+            }
+
+            // in the same column
+            if (Head.Y == Tail.Y)
+            {
+                if (Head.Y - Tail.Y >= 2)
+                {
+                    Tail.Y++;
+                }
+                else if (Head.Y - Tail.Y <= 2)
+                {
+                    Tail.Y--;
+                }
+            }
+
+            // diagonally adjacent - top left
+            if (Head.X < Tail.X && Head.Y < Tail.Y)
+            {
+                Tail.X--;
+                Tail.Y--;
+            }
+
+            // diagonally adjacent - top right
+            if (Head.X > Tail.X && Head.Y < Tail.Y)
+            {
+                Tail.X++;
+                Tail.Y--;
+            }
+
+            // diagonally adjacent - bottom left
+            if (Head.X < Tail.X && Head.Y > Tail.Y)
+            {
+                Tail.X--;
+                Tail.Y++;
+            }
+
+            // diagonally adjacent - bottom right
+            if (Head.X > Tail.X && Head.Y > Tail.Y)
+            {
+                Tail.X++;
+                Tail.Y++;
+            }
+
+            if (!TailPositions.Contains(Tail)) TailPositions.Add(new Position(Tail.X, Tail.Y));
+
+        }
+        
     }
 
-    public record Position(int X, int Y)
-    { 
+    public class Position : IEquatable<Position> 
+    {        
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;  
+        }
 
-    }
+        public bool Equals(Position? other)
+        {
+            if (other == null) return false;
+            return X == other.X && Y == other.Y;
+        }
+
+        public override string ToString()
+        {
+            return $"{X}, {Y}";
+        }
+
+        public bool IsAdjacent(Position item)
+        {
+            //equal
+            if (X == item.X && Y == item.Y) return true;
+
+            // in the same row or column
+            if (X == item.X) return (Y == item.Y + 1 || Y == item.Y - 1);
+            if (Y == item.Y) return (X == item.X + 1 || X == item.X - 1);
+
+            // diagonally adjacent
+            return (X == item.X - 1 && Y == item.Y - 1) || // top left
+                   (X == item.X + 1 && Y == item.Y - 1) || // top right                   
+                   (X == item.X - 1 && Y == item.Y + 1) || // bottom left
+                   (X == item.X + 1 && Y == item.Y + 1);   // bottom right
+        }        
+
+    }   
 
 }
