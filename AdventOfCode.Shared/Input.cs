7763971c142230ed3d06287a30956463c57d8f36
Craig Findlater <craig.findlater@outlook.com>
2023-12-01 23:15:00 +1300

    advent of code 2023 day 01

---
+++ AdventOfCode.Shared/Input.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace AdventOfCode.Shared;
+
+public static class Input
+{
+    public static string[] ReadDayInput(int day) =>
+        File.ReadAllLines($"{Environment.CurrentDirectory}\\day{day:00}\\input-day{day:00}.txt");
+
+    public static string[] ReadDayInput(int day, int part) =>
+        File.ReadAllLines($"{Environment.CurrentDirectory}\\day{day:00}\\input-day{day:00}-{part}.txt");
+}
---
+++ AdventOfCode2023/Program.cs
@@ -0,0 +1,6 @@
+﻿using AdventOfCode2023.Day01;
+
+Day01.Part1();
+Day01.Part2();
+
+Console.ReadLine();
